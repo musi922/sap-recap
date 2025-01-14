596184e31dac46b8a808e62194882404abe59f13
Richard Musime <richardmusime6@gmail.com>
2025-01-14 09:35:42 +0200

    added the custom button

--- webapp/control/CustomButton.js
+++ webapp/control/CustomButton.js
@@ -2,15 +2,22 @@ sap.ui.define(["sap/ui/core/Control"],(Control)=>{
     return Control.extend("saprecap.control.CustomButton",{
         metadata:{
             properties:{
-                text:{type:"string", defaulValue:""},
-                style:{type:"string", defaulValue:""},
-                class:{type:"string",defaulValue:""}
-            }
+                text:{type:"string", defaultValue:""},
+                style:{type:"string", defaultValue:""},
+                class:{type:"string",defaultValue:""}
+            },
+            events:{press:{}}
         },
         renderer:{
             render(oRM,oControl){
-                oRM.openStart("")
-            }
+                oRM.openStart("button",oControl).class("customButton").class(oControl.getClass()).attr("style",oControl.getStyle()).openEnd()
+                oRM.text(oControl.getText())
+                oRM.close("button")
+            },
+           
+        },
+        onclick(){
+            this.firePress()
         }
     })
 })
--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -4,6 +4,11 @@ sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController
 	return BaseController.extend("saprecap.controller.Main", {
 		sayHello: function () {
 			MessageBox.show("Hello World!");
+		},
+		onButtonPress: function(){
+			MessageBox.show("Hello World!");
+			console.log("Ddddd");
+			
 		}
 	});
 });
