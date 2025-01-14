bfe7d50aa146d4743577e8f424d0ac8b11e409fa
Richard Musime <richardmusime6@gmail.com>
2025-01-14 12:21:24 +0200

    added the one way binding

--- webapp/controller/About.controller.js
+++ webapp/controller/About.controller.js
@@ -1,10 +1,15 @@
 sap.ui.define([
     "./BaseController", 
-], function (BaseController) {
+    "sap/ui/model/json/JSONModel"
+], function (BaseController,JSONModel) {
     "use strict";
 
     return BaseController.extend("saprecap.controller.About", {
-
+        onInit(){
+            const oModel = new JSONModel();
+            oModel.loadData("model/data.json")
+            this.getView().setModel(oModel,"userModel")
+        },
         onHomePress() {
             this.getRouter().navTo("main");
             console.log("dd");
